c08b99351e65dbb24f298acca3ccec0357b8c9b4
Aakash Shah <subs@appsparkler.com>
2021-03-20 15:32:30 +0530

    reverted bg-color for button

--- src/shared/styles.js
+++ src/shared/styles.js
@@ -11,7 +11,7 @@ export const background = {
 
 export const color = {
   // Palette
-  primary: "orangered", // coral
+  primary: "#EC5685", // coral
   secondary: "#1EA7FD", // ocean
   tertiary: "#DDDDDD",
 
