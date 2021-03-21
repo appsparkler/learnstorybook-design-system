40bc58e7f9c4abfd74d91c0b9034c76ddda38098
Aakash Shah <subs@appsparkler.com>
2021-03-21 10:14:18 +0530

    changed color to test Storybook

--- src/shared/styles.js
+++ src/shared/styles.js
@@ -11,7 +11,7 @@ export const background = {
 
 export const color = {
   // Palette
-  primary: "#EC5685", // coral
+  primary: "red", // coral #EC5685
   secondary: "#1EA7FD", // ocean
   tertiary: "#DDDDDD",
 
